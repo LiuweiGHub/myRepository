bd6173543bfec113a916135e353fa51a6bf459b8
liuwei <18801257172@163.com>
2018-07-06 15:40:22 +0800

    add learn plan

--- RabbitMq/python/alert_consumer.py
+++ RabbitMq/python/alert_consumer.py
@@ -10,7 +10,6 @@ def send_mail(recipients, subject, message):
     smtp_server.sendmail("alerts@ourcompany.com", recipients, headers + str(message))
     smtp_server.close()
 
-# 通知处理程序
 def critical_notify(channel, method, header, body):
     """Sends CRITICAL alerts to administrators via e-mail"""
     EMAIL_RECIPS = ["ops.team@ourcompany.com",]
@@ -27,7 +26,6 @@ def rate_limit_notify(channel, method, herader, body):
     send_mail(EMAIL_RECIPS, "RATE LIMIT ALERT Text: %s " + "Recipients: %s") % (str(message), sre(EMAIL_RECIPS))
     channel.basic_ack(delivert_tag=method.delivert_tag)
 
-# 代理配置
 if __name__ == "__main__":
     AMQP_SERVER = "localhost"
 
@@ -36,22 +34,19 @@ AMQP_PASS = "alertme"
 AMQP_VHOST = "/"
 AMQP_EXCHANGE = "alerts"
 
-# 建立到代理的连接
 creds_broker = pika.PlainCredentials(AMQP_USER, AMQP_PASS)
 conn_params = pika_ConnectionParameters(AMQP_SERVER, virtual_host = AMQP_VHOST, credentials = creds_broker)
 conn_broker = pika.BlockingConnnection(conn_params)
 channel = conn_broker.channel()
 
-channel.exchange_declare(exchange = AMQP_EXCHANGE, type = "topic", auto_delete = False)  # 声明topic类型的交换器， auto_delete = False 意味着当最后一个消费者断开连接后交换器仍然存在
+channel.exchange_declare(exchange = AMQP_EXCHANGE, type = "topic", auto_delete = False)  
 
-# 为告警topic声明并绑定队列和交换器
 channel.queue_declare(queue = "critical", auto_delete = False)
 channel.queue_bind(queue = "critical", exchange = "alerts", routing_key = "critical.*")
 
 channel.queue_declare(queue = "rate_limit", auto_delete = False)
 channel.queue_bind(queue = "rate_limit", exchange = "alerts", routing_key = "*.rate_limit")
 
-# 设置消费者订阅并启动监听循环实现告警
 channel.basic_consume(critical_notify, queue = "critical", no_ack = False, consumer_tag = "critical")
 channel.basic_consume(rate_limit_notify, queue = "rate_limit", no_ack = False, consumer_tag = "rate_limit")
 
